5175a30650adb6e45f4160a630c043a2bee6deb6
Prerna <prerna1998.ps@gmail.com>
2019-09-04 23:14:06 +0530

    Staircase, right aligned

---
+++ staircase.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the staircase function below.
+void staircase(int n) {
+    for(int i=n;i>0;i--){
+        for(int j=1;j<=n;j++){
+            if(j<i)
+                cout<<" ";
+            else
+                cout<<"#";
+        }
+    cout<<endl;
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    staircase(n);
+
+    return 0;
+}
