b90a9aafb870148ee39e127e83a16e0d1eee3d21
Prerna <prerna1998.ps@gmail.com>
2019-09-08 20:10:07 +0530

    Sequence or permutation equation

---
+++ sequenceOrPermutationEquation.cpp
@@ -0,0 +1,104 @@
+/*
+
+***
+Find the Question here :)
+https://www.hackerrank.com/challenges/permutation-equation/problem
+
+Solution by : Prerna Sharma,2019
+prerna1998.ps@gmail.com
+Happy Coding! 
+***
+
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the permutationEquation function below.
+vector<int> permutationEquation(vector<int> p) {
+    vector<int> returnVector;
+    returnVector=p;
+    for(int i=0;i<p.size();i++){
+        int tempVar1=0,tempVar2=0;
+        for(int j=0;j<p.size();j++){
+            if(p[j]==i+1)
+                tempVar1=j+1;
+        }
+        
+        for(int k=0;k<p.size();k++){
+            if(tempVar1==p[k])
+                 tempVar2=k+1;
+        }
+        returnVector[i]=tempVar2;
+    }
+    return returnVector;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string p_temp_temp;
+    getline(cin, p_temp_temp);
+
+    vector<string> p_temp = split_string(p_temp_temp);
+
+    vector<int> p(n);
+
+    for (int i = 0; i < n; i++) {
+        int p_item = stoi(p_temp[i]);
+
+        p[i] = p_item;
+    }
+
+    vector<int> result = permutationEquation(p);
+
+    for (int i = 0; i < result.size(); i++) {
+        fout << result[i];
+
+        if (i != result.size() - 1) {
+            fout << "\n";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
