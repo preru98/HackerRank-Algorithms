697d974925d7fcad0f4a525f34f4d145493b7b45
Prerna <prerna1998.ps@gmail.com>
2020-01-07 01:41:03 +0530

    More efficient

--- appendAndDelete.cpp
+++ appendAndDelete.cpp
@@ -9,40 +9,27 @@ string appendAndDelete(string s, string t, int k) {
     int s_length=s.length();
     int t_length=t.length();
 
-    
+    int numDel;
+    int numAppend;
+
     int minLength=s_length;
-    if(s==t)
+    if(t_length < s_length)
+        minLength=t_length;   
+
+    for(int i=0;i<minLength;i++){
+        if(s[i]==t[i])
+            count++;
+        else break;
+    }
+    
+    numDel=s_length-count;
+    numAppend=t_length-count;
+    
+    int operation=numDel+numAppend;
+    if((k>=(s_length+t_length)) || (operation<k && ((k-operation)%2==0)))
        return "Yes";
     else
-        if(t_length < s_length)
-            minLength=t_length;   
-
-        for(int i=0;i<minLength;i++){
-            if(s[i]==t[i])
-                count++;
-            else break;
-        }
-        int numDel=s_length-count;
-        int numEmptyDel=0;
-        int numAppend=t_length-count;
-
-        cout<<s_length;cout<<endl;
-        cout<<t_length;cout<<endl;
-        cout<<count;cout<<endl;
-        cout<<numDel;cout<<endl;
-        cout<<numEmptyDel;cout<<endl;
-        cout<<numAppend;
-    
-        if(numDel==s_length)
-            if(numDel+numAppend<k)
-                numEmptyDel=k-numDel+numAppend;
-        else 
-            numDel==0;
-        if(k==(numDel+numEmptyDel+numAppend))
-            return "Yes";
-        else 
-            return "No";
-
+        return "No";
 }
 
 int main()
